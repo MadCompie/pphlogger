8ad0eebf5f950f44ae4ec4eeaf8900bf1cb053fe
Tarmo Sergo <angerjas@tmkk.ee>
2017-04-27 20:44:51 +0300

    mysql_result fix

--- include/get_csscolors.inc.php
+++ include/get_csscolors.inc.php
@@ -4,9 +4,14 @@
 /* get the columns names and fill them into an array */
 # list_fields not present in mysqli
 #$res = mysqli_list_fields(PPHL_DB_NAME, PPHL_TBL_CSS);
-
+global $link;
 $res = mysqli_query($link, "SHOW COLUMNS FROM ".PPHL_TBL_CSS.";");
-for($i = 0; $i < mysqli_num_fields($res); $i++) $csscolors[$i] = mysqli_field_name($res, $i);
+#for($i = 0; $i < mysqli_num_fields($res); $i++) $csscolors[$i] = mysqli_field_name($res, $i);
+for($i = 0; $i < mysqli_num_fields($res); $i++) 
+{
+    $temp = mysqli_fetch_field($res);
+    $csscolors[$i] = $temp->name;
+}
 
 /* fill the css color array */
 $sql = "SELECT * FROM ".PPHL_TBL_CSS." WHERE id = $cssid";
@@ -21,8 +26,10 @@ if (!@mysqli_num_rows($res)) {
 		${$csscolors[$i]} = mysqli_result($res2, 0, $i); //get all colors
 	}
 } else {
+	$temp = mysqli_fetch_row($res);
 	for($i = 3; $i < $cnt_csscolors; $i++) {
-		${$csscolors[$i]} = mysqli_result($res, 0, $i); //get all colors
+#		${$csscolors[$i]} = mysqli_result($res, 0, $i); //get all colors
+		${$csscolors[$i]} = $temp[$i]; //get all colors
 	}
 }
 ?>
